fb72db652b7cfef6018d39da734e99f19b84b052
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-02-04 01:43:45 +0100

    migracije

---
+++ bioskop/database/migrations/2023_02_04_002952_create_rezervacijes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rezervacijes', function (Blueprint $table) {
+            $table->id();
+            $table->string('datum');
+            $table->integer('broj_karata');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rezervacijes');
+    }
+};
